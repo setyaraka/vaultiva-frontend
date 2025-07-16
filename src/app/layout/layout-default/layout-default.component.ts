ff37c1f2a1a7dd8b133f11fbc2545869fcd9b281
setya.raka <setya.raka@elefin.id>
2025-07-16 09:21:52 +0700

    cleaning sidebar and logout button position

--- src/app/layout/layout-default/layout-default.component.ts
+++ src/app/layout/layout-default/layout-default.component.ts
@@ -3,17 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { AuthService } from '../../core/services/auth.service';
 import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-layout-default',
   standalone: true,
   imports: [
+    CommonModule,
+    RouterModule,
     RouterOutlet,
     NzLayoutModule,
     NzMenuModule,
-    NzIconModule
+    NzIconModule,
+    NzButtonModule
   ],
   templateUrl: './layout-default.component.html',
   styleUrl: './layout-default.component.css'
@@ -21,13 +28,13 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 export class LayoutDefaultComponent {
   isCollapsed = false;
 
-  constructor (
+  constructor(
     private authService: AuthService,
     private message: NzMessageService
   ) {}
 
   logout() {
     this.authService.logout();
-    this.message.success('Logout Success')
+    this.message.success('Logout Success');
   }
 }
